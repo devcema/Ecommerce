a54ffbf6e61dc6ca322f519d47697a5dfabad106
Fred <48894599+devcema@users.noreply.github.com>
2024-07-12 12:57:55 +0000

    implemented auth controller

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
+require('express-async-errors')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const authRouter = require('./routes/authRouter')
 
 const connectDb = require('./db/connect')
 require('dotenv').config()
 
 const notFoundMiddleware = require('./middlewares/not-found')
 const errorHandlerMiddleware = require('./middlewares/error')
+
+app.use(cookieParser(process.env.JWT_SECRET))
+app.use(express.json())
+app.use(morgan('tiny'))
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome gee')
+})
+app.use('/api/v1/auth', authRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
@@ -14,6 +28,7 @@ const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
+    await connectDb(process.env.MONGO_URI)
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}`)
     })
---
+++ controllers/authController.js
@@ -0,0 +1,57 @@
+const User = require('../models/UserModel')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../error')
+const { attachCookiesToResponse } = require('../utils')
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body
+
+  const emailExists = await User.findOne({ email: email })
+
+  if (emailExists) {
+    throw new BadRequestError('Email already exists')
+  }
+
+  const isFirstUser = (await User.countDocuments({})) === 0
+
+  const role = isFirstUser ? 'admin' : 'user'
+
+  const user = await User.create({ name, email, password, role })
+
+  const tokenUser = { name: user.name, userId: user._id, role: user.role }
+
+  attachCookiesToResponse({ res, user: tokenUser })
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser })
+}
+
+const login = async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    throw new UnauthenticatedError('Invalid email or password')
+  }
+
+  const isMatch = await user.verifyPassword(password)
+
+  if (!isMatch) {
+    throw new UnauthenticatedError('Invalid email or password')
+  }
+
+  const tokenUser = { name: user.name, userId: user._id, role: user.role }
+
+  attachCookiesToResponse({ res, user: tokenUser })
+
+  res.status(StatusCodes.OK).json({ tokenUser })
+}
+
+const logout = async (req, res) => {
+  res.cookie('token', null, {
+    expires: new Date(Date.now()),
+  })
+  res.status(StatusCodes.OK).json({ msg: 'Successfully logged out' })
+}
+
+module.exports = { register, login, logout }
